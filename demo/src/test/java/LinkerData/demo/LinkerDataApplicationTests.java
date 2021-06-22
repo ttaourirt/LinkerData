953a2e84bae33def7c056fcf285ed3d286b67f09
taoufik <taourirt@live.fr>
2021-06-22 12:15:06 +0200

    First Commit created with Spring

---
+++ demo/src/test/java/LinkerData/demo/LinkerDataApplicationTests.java
@@ -0,0 +1,13 @@
+package LinkerData.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkerDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
